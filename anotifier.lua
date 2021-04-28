736cec183816bf04f1685d34bfadcaac3a46bbd0
Shewer Lu <shewer@gmail.com>
2021-04-29 06:14:16 +0800

    add delete_notifier  local notifier_name
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- anotifier.lua
+++ anotifier.lua
@@ -20,88 +20,94 @@ function Notifier:setup(notifier)
   self:reset()
   self._list=metatable()
   self._mainfunc=self:_gener_mainfunc(self._list)
-  self:connect(notifier) 
-end 
+  self:connect(notifier)
+end
 
 
 function Notifier:_gener_mainfunc(list)
   return function(...)
     for _,ffunc in ipairs(list) do
-      if type(ffunc) == "function" and ffunc(...) then 
+      if type(ffunc) == "function" and ffunc(...) then
         break
       end
     end
   end
-end 
+end
 
 function Notifier:disconnect()
-  if self._connection  then 
+  if self._connection  then
     self._connection:disconnect()
     self._connection=nil
-  end 
+  end
 end
 function Notifier:connect(notifier)
   self:disconnect()
   self._connection= self._mainfunc and notifier:connect( self._mainfunc ) or nil
-end 
+end
 
 function Notifier:reset()
   self:disconnect()
   self._list=nil
-end 
+end
 function Notifier:append(func)
   self._list:insert(func)
-  return func
 end
 function Notifier:remove(func)
   local _,index=self._list:find(func)
   self._list:remove(index)
-  return  index and self._list:remove(index) 
-end 
+  return  index and self._list:remove(index)
+end
 function Notifier:funcs()
   return self._list
-end 
+end
 function Notifier:callback(...)
   self._mainfunc(...)
-end 
+end
 
+local notifier_name= {
+  commit = "commit_notifier",
+  select = "select_notifier",
+  update = "update_notifier",
+  delete = "delete_notifier",
+  option = "option_update_notifier",
+  property = "property_update_notifier",
+  unhandled = "unhandled_key_notifier",
+}
 
 
 local Notifiers=Class("Notifiers")
 
 function Notifiers:_initialize(ctx)
-    self._key= metatable({"commit","select","update","option","property","unhandled" }) 
-    if ctx then 
+    if ctx then
       self:setup(ctx)
-    end 
+    end
     return self
 end
 function Notifiers:setup(ctx)
-    self.commit=Notifier(    ctx.commit_notifier         )
-    self.select=Notifier(    ctx.select_notifier         )
-    self.update=Notifier(    ctx.update_notifier         )
-    self.option=Notifier(    ctx.option_update_notifier  )
-    self.property=Notifier(  ctx.property_update_notifier)
-    self.unhandled=Notifier( ctx.unhandled_key_notifier  ) 
-end 
+  for k,v in pairs(notifier_name) do
+    self[k] = Notifier( ctx[v] )
+  end
+  if log then
+     log.error( "------ delete notifier " .. tostring(ctx) .. "---" .. tostring(self.delete))
+  end
+end
 function Notifiers:reset()
-  self._key:each( function(elm) 
-     self[elm]:reset()
-     self[elm]=nil
-   end )
-
-end 
+  for k,v in pairs(notifier_func) do
+    self[k]:reset()
+    self[k]=nil
+  end
+end
 
 function Notifiers:connect()
-  self._key:each( function(elm) 
-     self[elm]:connect()
-   end )
-end 
+  for k,v in pairs(notifier_func) do
+    self[k]:connect()
+  end
+end
 function Notifiers:disconnect()
-  self._key:each( function(elm) 
-     self[elmr]:disconnect()
-   end )
-end 
+  for k,v in pairs(notifier_func) do
+    self[k]:disconnect()
+  end
+end
 
 return Notifiers
 
